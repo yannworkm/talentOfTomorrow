70054e949ae00b36b464f51442f51b751e4d1fda
Ludovic PERONNET <ludovic.peronnet@gmail.com>
2019-11-26 10:42:50 +0100

    Ajout des dictionnaires de poste

--- js/dictionnaire.js
+++ js/dictionnaire.js
@@ -1,4 +1,135 @@
-// FUNCTION JS A VENIR
+var Boulanger = {
+  "Poste" : "Boulanger",
+  "Entreprise": "Boulangerie Marie Blachère",
+  "Compétences" : {
+    "Technique" : {
+      "1" : "Pâte" ,
+      "2" : "Viennoiserie" ,
+      "3" : "Levain dur" ,
+      "4" : "Pâtisserie"
+    },
+    "Methodologique" : {
+      "1" : "Fabrication" ,
+      "2" : "Cuisson"
+    },
+    "Humaine" : {
+      "1" : "Ambitieux" ,
+      "2" : "Rigoureux" ,
+      "3" : "Accueillant" ,
+      "4" : "Chaleureux"
+    }
+  },
+  "Localisation": "Voiron",
+  "Description": "Nous recherchons actuellement un boulanger pour un CDI."
+};
+
+var IT_Infra = {
+  "Poste" : "Architecte infrastructure IT",
+  "Entreprise": "",
+  "Compétences" : {
+    "Technique" : {
+      "1" : "Windows" ,
+      "2" : "Linux" ,
+      "3" : "Serveur" ,
+      "4" : "NAS" ,
+      "5" : "Messagerie" ,
+      "6" : "TCP"
+    },
+    "Methodologique" : {
+      "1" : "Active directory" ,
+      "2" : "Sauvegarde" ,
+      "3" : "Support" ,
+      "4" : "Assistance"
+    },
+    "Humaine" : {
+      "1" : "Débrouillard" ,
+      "2" : "Polyvalent" ,
+      "3" : "Responsable"
+    }
+  },
+  "Localisation": "Grenoble",
+  "Description": "Nous recherchons actuellement un architecte en infrastructure IT. Ce poste débutera par un CDD de six mois puis un CDI"
+};
+
+var IT_Dev = {
+  "Poste" : "Ingénieur logiciel",
+  "Entreprise": "TRIXELL",
+  "Compétences" : {
+    "Technique" : {
+      "1" : "Java" ,
+      "2" : "C++" ,
+      "3" : "Python"
+    },
+    "Methodologique" : {
+      "1" : "Agile" ,
+      "2" : "Visual Studio" ,
+      "3" : "Test unitaires" ,
+      "4" : "Jenkins"
+    },
+    "Humaine" : {
+      "1" : "Accessible" ,
+      "2" : "Déterminé" ,
+      "3" : "Décidé" ,
+      "4" : "Méthodique"
+    }
+  },
+  "Localisation": "Moirans",
+  "Description": "Nous recherchons actuellement un developpeur pour un CDI."
+};
+
+var Commercial = {
+  "Poste" : "Commercial",
+  "Entreprise": "Carrefour",
+  "Compétences" : {
+    "Technique" : {
+      "1" : "Négociations" ,
+      "2" : "Gérance" ,
+      "3" : "Ventes"
+    },
+    "Methodologique" : {
+      "1" : "Commercial B to B" ,
+      "2" : "Analyse" ,
+      "3" : "Organisation" ,
+      "4" : ""
+    },
+    "Humaine" : {
+      "1" : "Efficace" ,
+      "2" : "Souriant" ,
+      "3" : "Spontané" ,
+      "4" : "Sociable"
+    }
+  },
+  "Localisation": "Gières",
+  "Description": "Nous recherchons actuellement un commercial. Ce poste débutera par un CDD de six mois puis un CDI"
+};
+
+var Secrétaire = {
+  "Poste" : "Secrétaire",
+  "Entreprise": "IBM",
+  "Compétences" : {
+    "Technique" : {
+      "1" : "Accueil physique" ,
+      "2" : "Accueil téléphonique" ,
+      "3" : "Standard"
+    },
+    "Methodologique" : {
+      "1" : "Rédaction de dossier," ,
+      "2" : "Notification de dossier" ,
+      "3" : "Gestion des courriers" ,
+      "4" : "Gestion administrative"
+    },
+    "Humaine" : {
+      "1" : "Accueillant " ,
+      "2" : "Agréable" ,
+      "3" : "Humain" ,
+      "4" : "Paisible"
+    }
+  },
+  "Localisation": "",
+  "Description": "Nous recherchons actuellement une secrétaire pour un CDI."
+};
+
+
 const competencesTech = [
   "Javascript",
   "Symfony",
